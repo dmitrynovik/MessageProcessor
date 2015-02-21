0c396701d088db3f73463bdecbb46233e1805d1d
dmitrynovik <cryinstone@gmail.com>
2015-02-22 00:09:14 +1100

    Serialization: Enum as string

--- MessageProcessor.Lib/Serializers/JsonSerializer.cs
+++ MessageProcessor.Lib/Serializers/JsonSerializer.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using MessageProcessor.Lib.Interfaces;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace MessageProcessor.Lib.Serializers
 {
@@ -10,7 +11,7 @@ namespace MessageProcessor.Lib.Serializers
         public Stream Serialize(IMessage message)
         {
             var fmt = message == null ? null : message.Formatted();
-            var json = JsonConvert.SerializeObject(fmt, Formatting.Indented);
+            var json = JsonConvert.SerializeObject(fmt, Formatting.Indented, new StringEnumConverter());
             return new MemoryStream(Encoding.UTF8.GetBytes(json));
         }
     }
